97f41c4558d5decdf0f6c3eefce2d4984970e882
jsyh1 <popcors369gmail.com>
2026-08-17 11:29:24 -0500

    Como usuario, quiero permitir seleccionar los operadores de suma, resta, multiplicación y división mediante los controles visibles, para permite construir operaciones matemáticas con los valores ingresados.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,5 +3,8 @@ module co.edu.poli.juego {
     requires javafx.fxml;
 
     opens co.edu.poli.juego to javafx.fxml;
+    opens co.edu.poli.controller to javafx.fxml;
+
     exports co.edu.poli.juego;
+    exports co.edu.poli.controller;
 }
