c092320069543ddca3500cc8726b1f5ea8eb136b
jsyh1 <popcors369gmail.com>
2026-09-07 11:31:31 -0500

    se crea boton compartir y se agrega al controlador para que cumpla con las acciones respectivas, se ve claramentente en el fxml

--- src/main/java/co/edu/poli/vista/App.java
+++ src/main/java/co/edu/poli/vista/App.java
@@ -6,7 +6,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -14,38 +13,30 @@ import javafx.stage.Stage;
  */
 public class App extends Application {
 
-    private static Scene scene;
+	private static Scene scene;
 
-    @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("juego"), 480, 480);
+	@Override
+	public void start(Stage stage) throws IOException {
+		scene = new Scene(loadFXML("juego"), 480, 480);
 
-        //Nombre de la ventana
-        stage.setTitle("All Ten");
-        //Logo
-        //stage.getIcons().add(
-        //new Image(App.class.getResourceAsStream("/co/edu/poli/juego/img/logo.jpg"))
-        
-        //);
-        
+		// Nombre de la ventana
+		stage.setTitle("All Ten");
+		stage.setScene(scene);
+		stage.show();
+	}
 
-        stage.setScene(scene);
-        stage.show();
-    }
+	static void setRoot(String fxml) throws IOException {
+		scene.setRoot(loadFXML(fxml));
+	}
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(
-            App.class.getResource("/co/edu/poli/juego/" + fxml + ".fxml")
-        );
+	private static Parent loadFXML(String fxml) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/co/edu/poli/juego/" + fxml + ".fxml"));
 
-        return fxmlLoader.load();
-    }
+		return fxmlLoader.load();
+	}
 
-    public static void main(String[] args) {
-        launch();
-    }
+	public static void main(String[] args) {
+		launch();
+	}
 
 }
